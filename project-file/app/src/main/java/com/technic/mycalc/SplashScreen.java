a6f90e7148f5e025cc10b0ee0ac41d495294f182
siambd <siam2cse@gmail.com>
2021-07-28 12:38:58 +0600

    first commit

---
+++ project-file/app/src/main/java/com/technic/mycalc/SplashScreen.java
@@ -0,0 +1,54 @@
+package com.technic.mycalc;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen<secondsDelayed> extends AppCompatActivity {
+    protected boolean _active = true;
+    protected int _splashTime = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        /////////////////////////////////////////////////////////
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        ///////////////////////////////////////////////////////
+
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if (_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch (Exception e) {
+
+                } finally {
+
+                    startActivity(new Intent(SplashScreen.this,
+                            ScientificCalculator.class));
+                    finish();
+                }
+            }
+        };
+        splashTread.start();
+    }
+}
